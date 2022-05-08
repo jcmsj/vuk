7232d6640128d9fdbdcd9d40f6b89e8d7764c6ae
Jean Carlo San Juan <sanjuan.jeancarlo@gmail.com>
2022-05-08 22:09:58 +0800

    removed MKCERT plugin for now and changed port

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import mkcert from 'vite-plugin-mkcert'
 // https://vitejs.dev/config/
 
-
 export default defineConfig({
-  server: { https: true },
-  plugins: [vue(), mkcert()]
+  server: { 
+    https: true,
+    open: "./index.html",
+    port: 80
+  },
+  plugins: [vue()]
 })
