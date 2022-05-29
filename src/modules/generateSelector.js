c842e8adf38bc6ec28725a894a0d3e246c2a9bfc
Jean Carlo San Juan <sanjuan.jeancarlo@gmail.com>
2022-05-29 15:21:11 +0800

    now escapes a period in an ID

--- src/modules/generateSelector.js
+++ src/modules/generateSelector.js
@@ -10,7 +10,7 @@ export default function generateSelector(el) {
   while (parent = el.parentNode) {
     let tag = el.tagName, siblings;
     path.unshift(
-      el.id ? `#${el.id}` : (
+      el.id ? `#${el.id.replace(".", "\\.")}` : (
         siblings = parent.children,
         [].filter.call(siblings, sibling => sibling.tagName === tag).length === 1 ? tag :
         `${tag}:nth-child(${1+[].indexOf.call(siblings, el)})`
