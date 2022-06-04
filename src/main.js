bd81130d7ddb36431bc69ebd466b55545d833c69
Jean Carlo San Juan <sanjuan.jeancarlo@gmail.com>
2022-06-04 21:38:48 +0800

    PWA attempt

--- src/main.js
+++ src/main.js
@@ -3,6 +3,13 @@ import App from './App.vue'
 import VueSimpleContextMenu from 'vue-simple-context-menu';
 import 'vue-simple-context-menu/dist/vue-simple-context-menu.css';
 
+import { registerSW } from "virtual:pwa-register";
+
+if ("serviceWorker" in navigator) {
+    // && !/localhost/.test(window.location)) {
+    registerSW();
+  }
+  
 const app = createApp(App);
 
 app.component('vue-simple-context-menu', VueSimpleContextMenu);
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import mkcert from 'vite-plugin-mkcert'
+import { VitePWA } from 'vite-plugin-pwa'
 // https://vitejs.dev/config/
 
 export default defineConfig({
@@ -12,5 +13,5 @@ export default defineConfig({
     open: "./index.html",
     port: 80
   },
-  plugins: [mkcert(), vue()]
+  plugins: [mkcert(), vue(), VitePWA()]
 })
