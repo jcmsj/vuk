66d376ad69cc26776b8dc95818537f59f77f297d
Jean Carlo San Juan <sanjuan.jeancarlo@gmail.com>
2022-06-05 10:50:54 +0800

    changed welcome message on mobile

--- src/components/WelcomePage.vue
+++ src/components/WelcomePage.vue
@@ -1,15 +1,24 @@
 <template>
 <div class="welcome-page">
-    Press: <br>
-    <span>
-    <kbd>B</kbd> - Show bookmarks
-    </span>
-    <span>
-    <kbd>C</kbd> - Show TOC
-    </span>
-    <span>
-    <kbd>F</kbd> - Show File explorer
-    </span>
+    <div
+        class="no-kb"
+    >
+        Tap on one of the buttons above.
+    </div>
+    <div
+        class="kb"
+    >
+        Press: <br>
+        <span>
+        <kbd>B</kbd> - Show bookmarks
+        </span>
+        <span>
+        <kbd>C</kbd> - Show TOC
+        </span>
+        <span>
+        <kbd>F</kbd> - Show File explorer
+        </span>
+    </div>
 </div>
 </template>
 <script setup>
@@ -18,13 +27,26 @@
 <style lang='sass' scoped>
 .welcome-page
     height: -webkit-fill-available
-    display: flex
-    justify-content: center
-    flex-direction: column
-    row-gap: 2vh
+
 kbd
     border: 1px solid black
     padding: 1px 1vw
     border-radius: 10%
+
+.kb, .welcome-page
+    display: flex
+    justify-content: center
+    flex-direction: column
+    row-gap: 2vh
+
+.kb
+    display:  none
+
+@media screen and (hover: hover)
+    .no-kb
+        display: none
+
+    .kb
+        display: flex
 
 </style>
