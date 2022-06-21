cf1bfe222326937122c0cdf4a7d38c383ef6dc25
Jean Carlo San Juan <sanjuan.jeancarlo@gmail.com>
2022-06-21 11:21:53 +0800

    formatted

--- src/main.js
+++ src/main.js
@@ -6,9 +6,9 @@ import 'vue-simple-context-menu/dist/vue-simple-context-menu.css';
 import { registerSW } from "virtual:pwa-register";
 
 if ("serviceWorker" in navigator) {
-    // && !/localhost/.test(window.location)) {
-    registerSW();
-  }
+  // && !/localhost/.test(window.location)) {
+  registerSW();
+}
   
 const app = createApp(App);
 
