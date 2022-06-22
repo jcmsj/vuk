dea3e5df5149c8c2be46fa7e821931c514f20fa6
Jean Carlo San Juan <sanjuan.jeancarlo@gmail.com>
2022-06-22 17:25:55 +0800

    added int indexing for reactive map

--- src/modules/reactives.js
+++ src/modules/reactives.js
@@ -3,7 +3,17 @@ import {reactive} from "vue"
 export function reactiveMap(obj = {}) {
     return reactive(
         Object.assign({
-            items: new Map()
+            items: new Map(),
+            at(n) {
+                let i = 0
+                for (const pair of this.items) {
+                    if (n == i++) {
+                        return pair
+                    }
+                }
+        
+                return null
+            }
         }, obj)
     )
 }
