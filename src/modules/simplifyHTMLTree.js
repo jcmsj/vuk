d44e601ee9f40f2e261293ba1b48b404aa38b040
Jean Carlo San Juan <sanjuan.jeancarlo@gmail.com>
2022-06-23 12:14:26 +0800

    Improved transformation algorithm to reduce runtime errors. The allowed tries has been reduced as a result.

--- src/modules/simplifyHTMLTree.js
+++ src/modules/simplifyHTMLTree.js
@@ -4,27 +4,25 @@ const type = {
 }
 /**
  * 
- * @param {HTMLElement} fragment
+ * @param {DocumentFragment} fragment
  * @returns {HTMLElement}
  * Simplifies HTML structure of chapters by keeping the nearest descendant that contains the actual text as well as removing containers for IMGs.
  */
 function simplifyHTMLTree(fragment) {
-
-    if (!(fragment instanceof HTMLElement))
+    if (!(fragment instanceof DocumentFragment))
         throw new TypeError("Not an element")
 
     let rootElem = fragment
     let end = false;
-    let tries = 100
+    let tries = 50
     while(!end && tries) {
         switch(rootElem.childElementCount) {
             case 0:
                 //If the deepest descendant is an IMG then it must be a cover page.
                 if (rootElem instanceof HTMLImageElement) {
                     end = type.cover;
-                } else {
-                    rootElem = rootElem.parentElement.nextElementSibling
-                }
+                } else 
+                    rootElem = rootElem.parentElement.nextElementSibling;
             break;
             case 1:
                 rootElem = rootElem.firstElementChild
@@ -38,33 +36,30 @@ function simplifyHTMLTree(fragment) {
 
         tries--;
     }
-    if (tries == 0) {
-        throw Error("inifnite loop:", rootElem)
-    }
-    if (end == type.cover) {
-        const div = document.createElement("div")
-        div.appendChild(rootElem)
-        return div;
-    }
+    if (tries == 0)
+        throw Error("inifnite loop:", rootElem);
 
-    for (const IMG of rootElem.querySelectorAll("IMG")) {
-        if (rootElem.isSameNode(IMG.parentElement)) {
-            continue
-        }
+    if (type.chapter) {
+        for (const IMG of rootElem.querySelectorAll("IMG")) {
+            if (rootElem.isSameNode(IMG.parentElement)) {
+                continue
+            }
 
-        let elem = IMG.parentElement;
-        //Locates the root's direct child that the IMG element descends from, then replaces it with the IMG. Note that the previous descendant is removed.
-        while(true) {            
-            if (rootElem.isSameNode(elem)) {
-                rootElem.replaceWith(IMG , elem)
-                break;
-            } else {
-                elem = elem.parentElement
+            let elem = IMG;
+            //Locates the root's direct child that the IMG element descends from, then replaces it with the IMG. Note that the previous descendant is removed.
+            while(elem = elem.parentElement) {  
+                if (rootElem.isSameNode(elem)) {
+                    rootElem.replaceWith(IMG , elem)
+                    break;
+                }
             }
         }
     }
+    
+    const div = document.createElement("div")
+    div.appendChild(rootElem)
 
-    return rootElem
+    return div
 }
 
 export default simplifyHTMLTree;
