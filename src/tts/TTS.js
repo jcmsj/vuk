54107c8fc8186c9cf02ff3b769e5e104c4930190
Jean Carlo San Juan <sanjuan.jeancarlo@gmail.com>
2022-07-03 13:57:34 +0800

    try to fix TTS

--- src/tts/TTS.js
+++ src/tts/TTS.js
@@ -1,2 +1,216 @@
+import { ref } from "vue";
+import Transformer from "./Transformer";
 import Word from "./Word";
+import { getSelectionText, isElementInViewport } from "/src/modules/helpers";
 import readAloud from "./narrator";
+import { className, validElems } from "./constants";
+import BookmarkController from "../Bookmarks/BookmarkController";
+import EnhancedEpub from "../modules/EnhancedEpub"
+import { refocus } from "../modules/helpers";
+export const isReading = ref(false)    
+let gElem = null
+
+/**
+ * @param {HTMLElement} lem 
+ */
+ export function isReadable(lem) {
+    return (lem instanceof HTMLElement) && validElems.RE.test(lem.tagName)
+}
+
+/**
+ * @param {Event} e 
+ */
+export function identifySpeechTarget(e) {
+    const elem = e.target;
+
+    if (elem.isSameNode(gElem) 
+    || !validElems.RE.test(elem.tagName)
+    ) 
+        return;
+
+    //const wasReading = isReading.value;
+
+    stopReading()
+    setSpeechTarget(elem)
+}
+
+/**
+ * @param {HTMLElement|null} chapterElem 
+ * @returns {HTMLElement}
+ */
+function find(chapterElem) {
+    if (chapterElem == null 
+    || !chapterElem.classList.contains(className.chapter)) {
+        
+        throw TypeError("Not a chapter element:", chapterElem);
+    }
+    
+    if (chapterElem.innerText.length == 0)
+        return find(chapterElem.nextElementSibling);
+
+    let target = chapterElem.querySelector(validElems.selector);
+    //todo: Traverse tree since the target may contain childs.
+    
+    return target;
+}
+
+/**
+ * @param {HTMLElement} elem 
+ * @returns success
+ */
+export function setSpeechTarget(elem) {
+    if (isReadable(elem)) {
+        Transformer.last = gElem;
+        gElem = elem;
+
+        console.log("R", gElem);
+        return true;
+    }
+
+    if (elem && elem.tagName == "FOOTER")
+        onBookEnd();
+
+    return false
+}
+
+/**
+ * @param {HTMLElement} elem 
+ */
+function spotTarget(elem) {
+    return setSpeechTarget(
+        find(
+            elem
+        )
+    )
+}
+export async function startReading() {
+    let txt = (gElem && gElem.innerText) || "";
+    txt = txt.slice(txt.indexOf(getSelectionText()))
+
+    try {
+        beforeSpeak(txt)
+    } catch(e) {
+        if (e instanceof TypeError) {
+            await EnhancedEpub.instance.next();
+            spotTarget(document.getElementById(EnhancedEpub.instance.id))
+            startReading();
+        }
+
+        throw e;
+    }
+}
+
+/**
+ * @param {string} txt 
+ * @returns Success
+ */
+function beforeSpeak(txt=gElem.innerText) {
+    if (txt.length == 0) {
+        //Todo: Add warning, since it may hint that there is an issue with 
+        upnext(gElem)
+        return false;
+    }
+
+    const {charIndex} = Transformer.transform(gElem, Word.index)
+    if (charIndex) {
+        txt = txt.slice(charIndex);
+    } else {
+        //The narrator is going to speak new text. 
+        Word.reset();
+    }
+
+    if(!isElementInViewport(gElem))
+        refocus(gElem);
+
+    const utterance = readAloud(txt)
+
+    const nextWord = e => {
+        Word.highlight(e, gElem)
+    }
+    utterance.onstart = nextWord
+    utterance.onboundary = nextWord
+
+    utterance.onend = () => {
+        upnext(gElem)
+    };
+
+    isReading.value = true;
+    return true;
+}
+
+export function stopReading() {
+    if (!isReading.value)
+        return
+
+    speechSynthesis.cancel();
+    isReading.value = false;
+    BookmarkController.saveProgress(gElem)
+    Transformer.revert()
+}
+
+export function toggleReading() {
+    isReading.value ? stopReading():startReading()
+}
+
+/**
+ * @param {HTMLElement} elem 
+ * @param {string} property 
+ */
+function upnext(elem, property = "nextElementSibling") {
+    let target = null
+    while(target == null) {
+        target = elem[property] 
+            || null;
+        elem = elem.parentElement
+    }
+
+    //When the element is empty, find next
+    if (target.innerText.length == 0) 
+        return upnext(target,property)
+
+    if (target.classList instanceof DOMTokenList
+    && target.classList.contains(className.chapter)
+    )
+        target = find(target);
+    
+    move(target);
+}
+
+/**
+ * @param {HTMLElement} target 
+ * Moves the visual indicator of the text being spoken.
+ */
+ function move(target) {
+    if (!isReading.value) {
+        return;
+    }
+
+    Word.reset();
+    Transformer.revert();
+    
+    if (setSpeechTarget(target)) {
+        beforeSpeak()
+    } else {
+        onBookEnd()
+    }
+}
+
+async function onBookEnd() {
+    try {
+        await EnhancedEpub.instance.next()
+    } catch(e) {
+        readAloud("There is no more text to read.")
+        throw Error("End of Book!") //Force an error | Halting problem
+    }
+    //TODO: do BookEndEvent
+}
+
+export function onBookLoaded() {
+    const maybeChapter = document.querySelector("." + className.chapter)
+    if (spotTarget(maybeChapter)) {
+        console.log("First chapter:", maybeChapter);
+        return true
+    }
+
+    return false
+}
