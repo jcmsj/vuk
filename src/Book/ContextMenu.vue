adf2a87ec590c50018d1f71e50d679e89c19dedb
Jean Carlo San Juan <sanjuan.jeancarlo@gmail.com>
2022-11-12 19:40:51 +0800

    Feat: new book context menu

---
+++ src/Book/ContextMenu.vue
@@ -0,0 +1,47 @@
+<template>
+    <q-menu touch-position auto-close>
+        <q-list>
+            <q-item v-for="item of menuItems" clickable :key="item.label" @click="item.cb">
+                <q-item-section avatar>
+                    <q-icon :name="item.icon">
+                    </q-icon>
+                </q-item-section>
+                <q-item-section>
+                    {{ item.label }}
+                </q-item-section>
+            </q-item>
+        </q-list>
+    </q-menu>
+</template>
+<script setup lang=ts>
+import { QMenu, QList, QItem, QItemSection, QIcon } from 'quasar';
+import { BookmarkController } from 'src/Bookmarks';
+import { stopReading, startReading } from 'src/TTS';
+import { narrator } from 'src/TTS/Narrator';
+
+const menuItems = [
+    {
+        label: "Read aloud",
+        icon: "campaign",
+        cb() {
+            stopReading()
+            startReading()
+        }
+    },
+    {
+        label: "Bookmark",
+        icon: "bookmark_add",
+        cb() {
+            BookmarkController.toggle(
+                narrator.target!
+            )
+        }
+    },
+    {
+        label: "Copy",
+        icon: "content_copy",
+        cb() { }
+    }
+]
+
+</script>
