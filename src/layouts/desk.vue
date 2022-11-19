7b90ab9a826c846c93c5262d2d22b3358fe452da
Jean Carlo San Juan <sanjuan.jeancarlo@gmail.com>
2022-11-19 14:18:21 +0800

    Feat: responsive layout

--- src/layouts/desk.vue
+++ src/layouts/desk.vue
@@ -1,5 +1,5 @@
 <template>
-  <q-tabs vertical align="left" switch-indicator shrink content-class="sidebar" v-model="tab" on-same-tab="deselect" active-color="black" 
+  <q-tabs :vertical="onDesk" align="left" switch-indicator shrink content-class="sidebar" v-model="tab" on-same-tab="deselect" active-color="black" 
   class="bg-secondary text-grey-7 shadow-2"
   >
     <q-tab name="browse" icon="folder" title="Browse" @click="toggleSelect('browse')" />
@@ -7,7 +7,7 @@
     <q-tab name="bookmarks" icon="bookmarks" title="Bookmarks" @click="toggleSelect('bookmarks')" />
     <q-tab name="config" icon="settings" title="Config" @click="toggleSelect('config')" />
   </q-tabs>
-  <div>
+  <main>
   <Transition name="slide" >
     <q-tab-panels v-model="tab" v-if="tab?.length" class="shadow-3">
       <q-tab-panel name="browse">
@@ -25,7 +25,7 @@
     </q-tab-panels>
   </Transition>
     <Live />
-  </div>
+</main>
 </template>
   
 <script setup lang=ts>
@@ -36,10 +36,13 @@ import { ref, watch } from 'vue'
 import TOCVue from 'src/TOC/TOC.vue';
 import { QTabs } from 'quasar';
 import SettingsPage from 'src/settings/SettingsPage.vue';
-import { useEventListener } from '@vueuse/core';
+import { useEventListener, useMediaQuery } from '@vueuse/core';
 const tab = ref<string | undefined>("")
 
 const isSelected = ref(false);
+
+/* if device has no touch screen */
+const onDesk = useMediaQuery("(any-pointer: fine) and (min-width: 1024px)");
 
 function toggleSelect(tabName: string) {
   console.log({ tabName });
@@ -86,26 +89,48 @@ useEventListener("keyup", e => {
 @use "../sass/scrollbar"
 #q-app
   display: grid
-  grid-template-columns: auto 1fr
+  height: 100dvh
+  height: 100vh
+  @include mobile
+    grid-template-areas: "book""nav"
+    grid-template-rows: 1fr auto
+
+  @include desk
+    grid-template-areas: "nav book"
+    grid-template-columns: auto 1fr
 </style>
 <style lang=sass scoped>
 .q-page-container
   width: 100%
 
   /* TODO: Class for d/vh usage */
+
+main
+  grid-area: book
 .q-tabs
   position: sticky
-  top: 0
-  height: 100dvh
-  height: 100vh /* Fallback */
+
+  grid-area: nav
+
+  @include mobile
+    bottom: 0
+
+  @include desk
+    top: 0
+    height: 100dvh
+    height: 100vh /* Fallback */
 
 .q-tab-panels
   z-index: 1
-  width: 30vw
   position: fixed
   height: 100dvh
   height: 100vh /* Fallback */
   resize: horizontal
+  width: 100%
+  
+  @include desk
+    width: 30vw
+
 .q-tab-panel
   padding: 1vw 1vh
 .q-tabs
@@ -117,6 +142,11 @@ useEventListener("keyup", e => {
 
 .slide-enter-from,
 .slide-leave-to 
-  transform: translateX(-30vw)
   opacity: 0
+
+  @include mobile
+    transform: translateY(30vh)
+  @include desk
+    transform: translateX(-30vw)
+
 </style>
