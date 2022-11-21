3d3218dbb3591c6d7e5ae87516eded527a897cb7
Jean Carlo San Juan <sanjuan.jeancarlo@gmail.com>
2022-11-21 16:25:34 +0800

    Feat: Versions component

--- src/settings/SettingsPage.vue
+++ src/settings/SettingsPage.vue
@@ -1,34 +1,19 @@
 <template>
     <q-list bordered>
         <q-item-label header>
-            Book config
+            Book
         </q-item-label>
         <VLoader />
-        <ThemeSwitcher />
         <q-separator />
         <q-item-label header>
-            Versions
+            Application
         </q-item-label>
-        <q-item>
-            <q-item-section>
-                Vuk v{{ version }}
-            </q-item-section>
-        </q-item>
-        <q-item>
-            <q-item-section>
-                Epub v{{ epubVersion }}
-            </q-item-section>
-        </q-item>
-        <q-item>
-            <q-item-section>
-                Quasar v{{ $q.version }}
-            </q-item-section>
-        </q-item>
+        <ThemeSwitcher />
+        <Versions />
     </q-list>
 </template>
 <script setup lang=ts>
-import { version } from "../../package.json"
-import {version as epubVersion} from "@jcsj/epub/package.json"
+import Versions from "./Versions.vue"
 import VLoader from "./loader.vue" 
 import ThemeSwitcher from "./ThemeSwitcher.vue"
 </script>
---
+++ src/settings/Versions.vue
@@ -0,0 +1,25 @@
+<template>
+    <q-separator />
+    <q-item-label header>
+        Versions
+    </q-item-label>
+    <q-item>
+        <q-item-section>
+            Vuk v{{ version }}
+        </q-item-section>
+    </q-item>
+    <q-item>
+        <q-item-section>
+            Epub v{{ epubVersion }}
+        </q-item-section>
+    </q-item>
+    <q-item>
+        <q-item-section>
+            Quasar v{{ $q.version }}
+        </q-item-section>
+    </q-item>
+</template>
+<script setup lang=ts>
+import { version } from "../../package.json"
+import { version as epubVersion } from "@jcsj/epub/package.json"
+</script>
