08151200150ec33bc9d42c6d32913045ede97bd5
Jean Carlo San Juan <sanjuan.jeancarlo@gmail.com>
2023-07-24 16:23:50 +0800

    navigation guards for external or missing ids

--- src/Library/anchorClicked.ts
+++ src/Library/anchorClicked.ts
@@ -1,16 +1,31 @@
-import { instance } from "../lib/EnhancedEpub"
+import { log } from "src/settings/DevMode";
 import { LoadMethod, loadMethod } from "./Load";
+import { instance } from "src/lib/EnhancedEpub";
 
-export async function anchorClicked(e: MouseEvent, id?:string) {
+export async function anchorClicked(e: MouseEvent, id?: string) {
   if (e.target instanceof HTMLAnchorElement) {
-    id = id ?? e.target.hash.split("#")[1];
+    id ??= e.target.hash.split("#")[1];
+
+    // Stop navigation when `id` is nullable
+    // Note: this doesnt prevent external links since this just checks for an id.
+
+
+    // Confirm navigation for external link
+    if (e.target.hostname !== window.location.hostname) {
+      if (window.confirm("Visit external site?")) {
+        window.open(e.target.href, "_blank")
+      } else {
+        e.preventDefault()
+      }
+      return;
+    }
+    log(`%c[Router] ${e.target.href}`, "color: lightgreen;", id,);
   }
 
-  if (id) {
-    if (loadMethod.value == LoadMethod.all) {
-      document.getElementById(id)?.scrollIntoView()
-    } else {
-      instance.value?.between({id})
-    }
+  if (loadMethod.value === LoadMethod.all) {
+    document.getElementById(id!)?.scrollIntoView({ behavior: "smooth" })
+  }  else {
+    instance.value?.between({ id })
   }
+
 }
