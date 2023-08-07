99105ad863bdbd19eb2e6aab4c67048b05362ba5
Jean Carlo San Juan <sanjuan.jeancarlo@gmail.com>
2023-08-08 00:52:06 +0800

    layout: epub imgs size in firefox matches chromium

--- src/Book/VPage.vue
+++ src/Book/VPage.vue
@@ -6,22 +6,25 @@
 <style lang="sass">
 /* Important: Unscoped */
 @use "../Bookmarks/style"
+
 div.chapter
     min-height: 110vh
     display: flex
     flex-direction: column
 
-    img, svg:has(image) /* Uses deep cause of v-html */
+    /* Uses is to increase specificity due to chrome vs firefox 
+     * :deep doesnt work https://github.com/vuejs/core/issues/6605
+     */
+    :is(img, svg:has(image))
         /* Sizing */
         object-fit: contain
         max-width: 80% !important
         max-height: 80vh !important
-
         /* Aligns center */
         margin-inline: auto
         display: block
 
-    h1, h2, h3, h4, h5, h6
+    :is(h1, h2, h3, h4, h5, h6)
         align-self: center
 
         //Patch for stylized chapter titles for now
@@ -30,8 +33,8 @@ div.chapter
 </style>
 <script setup lang="ts">
 interface Page {
-    id:string,
-    html:string
+    id: string,
+    html: string
 }
-const {page} = defineProps<{page:Page}>();
+const { page } = defineProps<{ page: Page }>();
 </script>
