3e495f5326f2ed896f9e33a773d07343460a0167
Jean Carlo San Juan <sanjuan.jeancarlo@gmail.com>
2023-08-26 23:21:25 +0800

    Save window rize

--- src-electron/electron-main.ts
+++ src-electron/electron-main.ts
@@ -4,10 +4,38 @@ import os from 'os';
 import { prepLeave } from './prepLeave';
 import { prepIPC } from './ipc';
 import { prepDev } from './prepDev';
+import Store from 'electron-store';
+import { WebContents } from 'electron';
 
 // needed in case process is undefined under Linux
 const platform = process.platform || os.platform();
 
+/**
+ * @see https://github.com/electron/electron/issues/526#issuecomment-1375534548
+ */
+function rememberWindowState(w: BrowserWindow) {
+  const store = new Store<{ bounds?: Electron.Rectangle }>()
+  const b = store.get('bounds');
+  if (b) {
+    w.setBounds(b)
+  } else {
+    w.maximize()
+  }
+
+  w.on('close', () => {
+    store.set('bounds', w.getBounds())
+  })
+}
+
+/**
+ * Restore pinch zoom
+ */
+function addZoom(c: WebContents) {
+  c.setVisualZoomLevelLimits(1, 3);
+  c.on("zoom-changed", (_, direction) => {
+    c.zoomFactor += 0.2 * (direction == "in" ? 1 : -1);
+  })
+}
 try {
   if (platform === 'win32' && nativeTheme.shouldUseDarkColors === true) {
     require('fs').unlinkSync(
@@ -34,17 +62,14 @@ function createWindow() {
       preload: path.resolve(__dirname, process.env.QUASAR_ELECTRON_PRELOAD),
     },
   });
-  mainWindow.maximize()
+  rememberWindowState(mainWindow);
   mainWindow.loadURL(process.env.APP_URL);
 
   prepDev(mainWindow);
   prepIPC();
   mainWindow.webContents.on('will-prevent-unload', prepLeave(mainWindow));
-  // Restore pinch zoom
-  mainWindow.webContents.setVisualZoomLevelLimits(1,3); 
-  mainWindow.webContents.on("zoom-changed", (_, direction) => {
-    mainWindow.webContents.zoomFactor += 0.2 * (direction == "in" ? 1:-1);
-  })
+  addZoom(mainWindow.webContents)
+
 }
 
 app.whenReady().then(createWindow);
