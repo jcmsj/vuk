f208c3d405c48e51b32cde26938e0edf4fc63217
Jean Carlo San Juan <sanjuan.jeancarlo@gmail.com>
2024-01-03 20:27:43 +0800

    update vue and disable vue devtools loading for now

--- src-electron/prepDev.ts
+++ src-electron/prepDev.ts
@@ -1,18 +1,18 @@
 import { BrowserWindow, app } from "electron";
 
 export function prepDev(mainWindow: BrowserWindow) {
-  if (process.env.DEBUGGING) {
-    // if on DEV or Production with debug enabled
-    // Note: Can't use static import as it fails in prod 
-    if (!app.isPackaged) {
-      import("electron-devtools-installer").then(({ default: installExtension, VUEJS_DEVTOOLS }) => {
-        installExtension(VUEJS_DEVTOOLS)
-          .then(name => console.log(`Added Extension:  ${name}`))
-          .catch(err => console.log('An error occurred: ', err));
-      })
-    }
-    return
-  }
+  // if (process.env.DEBUGGING) {
+  //   if on DEV or Production with debug enabled
+  //   Note: Can't use static import as it fails in prod 
+  //   if (!app.isPackaged) {
+  //     import("electron-devtools-installer").then(({ default: installExtension, VUEJS_DEVTOOLS }) => {
+  //       installExtension(VUEJS_DEVTOOLS)
+  //         .then(name => console.log(`Added Extension:  ${name}`))
+  //         .catch(err => console.log('An error occurred: ', err));
+  //     })
+  //   }
+  //   return
+  // }
 
   // we're on production; no access to devtools pls
   mainWindow.webContents.on('devtools-opened', mainWindow?.webContents.closeDevTools);
